cd72290d87ced5e9e2e50b38c75cd80d4e78f144
Cédric Pontet <cedric.pontet@gmail.com>
2022-02-24 22:05:28 +0100

    Added public URL to server config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   emitErrors: false,
   proxy: env.bool('IS_PROXIED', true),
+  url: env('PUBLIC_URL', 'https://api.example.com'),
   cron: {
     enabled: env.bool('CRON_ENABLED', false),
   },
