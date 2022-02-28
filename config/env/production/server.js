2646b6f2d9e910f337bfcdd8a9f205c3e0c1b293
Cédric Pontet <cedric.pontet@gmail.com>
2022-02-28 20:30:22 +0100

    Commented unnecessary config items

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  emitErrors: false,
-  proxy: env.bool('IS_PROXIED', true),
-  url: env('PUBLIC_URL', 'https://api.example.com'),
+  // emitErrors: false,
+  // proxy: env.bool('IS_PROXIED', true),
+  // url: env('PUBLIC_URL', 'https://api.example.com'),
   cron: {
     enabled: env.bool('CRON_ENABLED', false),
   },
