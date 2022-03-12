5240569768b179b0964f7275b4c140a3713e2a55
Cédric Pontet <cedric.pontet@gmail.com>
2022-03-12 14:19:12 +0100

    Implemented find by slug

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -6,4 +6,26 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::event.event');
+module.exports = createCoreController('api::event.event', ({ strapi }) =>  ({
+
+  async findOne(ctx) {
+    const { slug } = ctx.params;
+    const { fields } = ctx.query;
+    const { populate } = ctx.query;
+    const { publicationState } = ctx.query;
+
+    const query = {
+      fields: fields,
+      filters: { slug: slug },
+      populate: populate,
+      publicationState
+    };
+
+    const entities = await strapi.entityService.findMany('api::event.event', query);
+    const entity = entities[0];
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  }
+
+}));
