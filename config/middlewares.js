5fa17ba9394c7b08ddcedad89e941845a4f88147
Cédric Pontet <cedric.pontet@gmail.com>
2022-03-22 17:21:14 +0100

    override the CSP settings for Azure Storage image provider

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,18 @@
-module.exports = [
+module.exports = ({
+  env
+}) => [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('STORAGE_ACCOUNT')}.blob.core.windows.net`],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
