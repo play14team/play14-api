734fe583e3b759322ac035b716b5e3ea82ef2ce1
Cédric PONTET <cpontet@agilepartner.net>
2023-01-15 11:27:45 +0100

    configured graphql for introspection in production

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,17 @@
 module.exports = ({ env }) => ({
+    'graphql': {
+      config: {
+          endpoint: '/graphql',
+          shadowCRUD: true,
+          playgroundAlways: true,
+          depthLimit: 7,
+          amountLimit: 100,
+          apolloServer: {
+              tracing: false,
+              introspection: true
+          }
+      }
+  },
   'ckeditor': true,
   'seo': {
     enabled: true,
