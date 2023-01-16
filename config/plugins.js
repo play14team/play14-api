e4b2f32bff8bd837eb6573e05c3fe4deb1575cdd
Cédric PONTET <cpontet@agilepartner.net>
2023-01-16 22:20:05 +0100

    increased graphql limits

--- config/plugins.js
+++ config/plugins.js
@@ -4,8 +4,8 @@ module.exports = ({ env }) => ({
           endpoint: '/graphql',
           shadowCRUD: true,
           playgroundAlways: true,
-          depthLimit: 7,
-          amountLimit: 100,
+          depthLimit: 10,
+          amountLimit: 1000,
           apolloServer: {
               tracing: false,
               introspection: true
