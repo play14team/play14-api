19d9275470a23af646f6609b44b4319fa2936360
Cédric PONTET <cpontet@agilepartner.net>
2023-02-13 22:26:58 +0100

    used sdk to geocode

--- bootstrap/geocode.js
+++ bootstrap/geocode.js
@@ -41,28 +41,25 @@ async function getCountryFromCity(city) {
   if (city == "bigone" || city == "online")
     return "";
 
-  if (city == "porto")
-    return "PT";
+  if (city === "porto") return "PT";
 
-  const response = await axios.get(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${city},.json?limit=5&access_token=${mapboxAccessToken}`
-  );
-  const data = response.data;
-  if (data && data.features.length > 0 && data.features[0].context) {
-    const countryCode = data.features[0].context[data.features[0].context.length-1].short_code;
-    return countryCode ? countryCode.toUpperCase() : "";
+  const geocodingService = mbxGeocoding({ accessToken: mapboxAccessToken });
+  const response = await geocodingService.forwardGeocode({
+    query: city,
+    types: [],
+    limit: 1
+  }).send();
+
+  const result = response.body.features[0];
+  if (result && result.id.includes("country")) {
+    return result.properties.short_code.toUpperCase();
+  }
+  if (result && result.context.length > 0) {
+    const context = result.context.filter(c => c.id.includes("country"))[0];
+    return (context && context.short_code.toUpperCase()) || null;
   }
 
-  const response2 = await axios.get(
-    `https://api.mapbox.com/geocoding/v5/mapbox.places/${city}%20city.json?limit=5&access_token=${mapboxAccessToken}`
-  );
-  const data2 = response2.data;
-  if (data2 && data2.features.length > 0 && data2.features[0].context) {
-    const countryCode = data2.features[0].context[data2.features[0].context.length-1].short_code;
-    return countryCode ? countryCode.toUpperCase() : "";
-  }
-
-  return "";
+  return null;
 }
 
 
