e011ad70a95d5f3e752157b50a9e4738738ea1fa
Cédric PONTET <cpontet@agilepartner.net>
2023-03-15 23:21:54 +0100

    added timezone

--- config/plugins.js
+++ config/plugins.js
@@ -26,16 +26,6 @@ module.exports = ({ env }) => ({
       }
     }
   },
-  'vercel-deploy': {
-    enabled: true,
-    config: {
-      deployHook: process.env.VERCEL_DEPLOY_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
-      teamFilter: process.env.VERCEL_DEPLOY_PLUGIN_TEAM_FILTER,
-      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-    }
-  },
   'strapi-plugin-populate-deep': {
     config: {
       defaultDepth: 3, // Default is 5
